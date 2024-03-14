ba0fab791ed03272243693011a83c25c9ae9b165
Eunbyul <89649741+EunByu1@users.noreply.github.com>
2024-03-14 19:55:22 +0900

    Update main.py

--- AICC/code/main.py
+++ AICC/code/main.py
@@ -4,11 +4,11 @@ import os
 # 1. mp4 -> mp3 변환
 ####################################################################
 # 파일 경로
-file_path = "C:/Users/win/Desktop/project/sampleVideo.mp4"
+file_path = "you are 'sampleVideo.mp4' file path"
 # 파일 이름 추출 (확장자 제외)
 file_name_without_ext = os.path.basename(file_path).rsplit('.', 1)[0]
 # 출력 파일 경로 생성 - "C:/Users/win/Desktop/data" 폴더 내에 같은 파일 이름으로 .mp3 확장자 사용
-output_path = f"C:/Users/win/Desktop/data/{file_name_without_ext}.mp3"
+output_path = f"'save file path'/{file_name_without_ext}.mp3"
 
 # 비디오 파일 로드
 clip = VideoFileClip(file_path)
