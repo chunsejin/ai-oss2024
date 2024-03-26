f7c00c636c318404d61da00b50715cf3459b120a
Junwon Lee <63298243+cpprhtn@users.noreply.github.com>
2024-03-26 16:37:40 +0900

    Update find_element.py

--- codes/find_element.py
+++ codes/find_element.py
@@ -1,9 +1,8 @@
 def find_element(lst, element):
-    
     for i in range(len(lst)):
-    if lst[i] == element
-        return i  
-    return -1 
+        if lst[i] == element: 
+            return i
+    return -1
 
 my_list = [1, 2, 3, 4, 5]
 element_to_find = 3
