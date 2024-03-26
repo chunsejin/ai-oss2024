be4c57e833d21f58c495ca02160cb3a3f55ccc59
Hwayoung Kim <147595460+keemhhyy@users.noreply.github.com>
2024-03-26 16:19:59 +0900

    feature: find_element

---
+++ codes/find_element.py
@@ -0,0 +1,11 @@
+def find_element(lst, element):
+    
+    for i in range(len(lst)):
+    if lst[i] == element
+        return i  
+    return -1 
+
+my_list = [1, 2, 3, 4, 5]
+element_to_find = 3
+
+print("인덱스:", find_element(my_list, element_to_find))
